05183de4b2352a80afe2755f0932ba4e444c4d52
mehedi-hassan-niloy <mehedihassanniloy07@gmail.com>
2023-03-27 15:29:40 +0600

    last commit

--- src/Componnents/Shop/Shop.jsx
+++ src/Componnents/Shop/Shop.jsx
@@ -4,11 +4,17 @@ import './Shop.css';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([])
     useEffect( () =>{
      fetch('products.json')
      .then(res => res.json())
      .then(data =>  setProducts(data))
     }, [])
+
+    const handelAddToCart = (product) =>{
+        const newCart = [...cart, product];
+        setCart(newCart);
+       }
 
     return (
         <div className='shop-container'>
@@ -17,11 +23,14 @@ const Shop = () => {
                 products.map(product => <Product
                 key={product.id}
                 product = {product}
+                handelAddToCart = {handelAddToCart}
                 ></Product>)
             }
             </div>
             <div className="cart-container">
-                <h4>order summary</h4>
+                <h4>Order summary</h4>
+                <p>Selected Items : {cart.length}</p>
+                
             </div>
         </div>
     );
